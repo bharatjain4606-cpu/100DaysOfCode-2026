9e7ad6e26c522d58d6c73eb01b4a4e987f425575
Vibhutiinegi <Vibhuti.29442@stu.upes.ac.in>
2026-06-17 13:30:19 +0530

    Day 3 Questions

--- DSA/VibhutiNegi_590029442/Day_3/Question1_LeetCode.c
+++ DSA/VibhutiNegi_590029442/Day_3/Question1_LeetCode.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 int* plusOne(int* digits, int size, int* returnSize) {
     for (int i = size - 1; i >= 0; i--) {
-        if (digits[i] < 9){
+        if (digits[i] < 9) {
             digits[i]++;
             *returnSize = size;
             return digits;
@@ -20,28 +17,4 @@ int* plusOne(int* digits, int size, int* returnSize) {
 
     *returnSize = size + 1;
     return ans;
-}
-
-int main() {
-    int n;
-
-    printf("Enter number of digits: ");
-    scanf("%d", &n);
-
-    int* digits = (int*)malloc(n * sizeof(int));
-
-    printf("Enter digits:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &digits[i]);
-    }
-
-    int returnSize;
-    int* result = plusOne(digits, n, &returnSize);
-
-    printf("Result: ");
-    for (int i = 0; i < returnSize; i++) {
-        printf("%d ", result[i]);
-    }
-
-    return 0;
 }
